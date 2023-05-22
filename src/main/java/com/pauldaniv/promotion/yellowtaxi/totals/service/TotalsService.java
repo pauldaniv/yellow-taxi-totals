4ee20a33217e06180ca2223906584c0564f16346
Pavlo <paulsegfault@gmail.com>
2023-05-22 23:47:23 +0300

    Make container wait

--- src/main/java/com/pauldaniv/promotion/yellowtaxi/totals/service/TotalsService.java
+++ src/main/java/com/pauldaniv/promotion/yellowtaxi/totals/service/TotalsService.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-
 @Slf4j
 @Service
 public class TotalsService {
 
     @Cacheable("itemCache")
     public String run() {
+        try {
+            Thread.sleep(120_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         String returningValue = String.valueOf(System.currentTimeMillis());
         log.info("msg=returning_value value={}", returningValue);
         return returningValue;
